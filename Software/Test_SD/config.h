08726f4d3175c1e4acddf7b3910d8f0355663784
Dexter Carpenter <dexter.carpenter03@gmail.com>
2022-02-15 13:54:10 -0800

    Create Test Script

---
+++ Software/Test_SD/config.h
@@ -0,0 +1,16 @@
+
+"{\
+	'general':\
+	{\
+		'name':'Device',\
+		'instance':1,\
+		'interval':5000,\
+		'print_verbosity':2\
+	},\
+	'components':[\
+		{\
+			'name':'SD',\
+			'params':[true,1000,11,'data',true]\
+		}\
+	]\
+}"
