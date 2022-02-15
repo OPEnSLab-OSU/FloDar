05f15fc1f9b3d064dbff0f790cfe922ce29f87e4
Dexter Carpenter <dexter.carpenter03@gmail.com>
2022-02-15 13:53:39 -0800

    Create 30 Second Script

---
+++ Software/30_sec/config.h
@@ -0,0 +1,35 @@
+"{\
+	'general':\
+	{\
+		'name':'Device',\
+		'instance':1,\
+		'interval':5000,\
+		'print_verbosity':2\
+	},\
+	'components':[\
+		{\
+			'name':'ADS1115',\
+			'params':'default'\
+		},\
+    {\
+      'name':'MAX31856',\
+      'params':'default'\
+    },\
+		{\
+			'name':'SD',\
+			'params':[true,1000,11,'data',true]\
+		},\
+		{\
+			'name':'DS3231',\
+			'params':'default'\
+		},\
+		{\
+			'name':'Interrupt_Manager',\
+			'params':[0]\
+		},\
+		{\
+			'name':'Sleep_Manager',\
+			'params':[true,false,1]\
+		}\
+	]\
+}"
