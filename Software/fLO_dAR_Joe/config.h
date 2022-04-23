e8c4adf3f1e50d0525294f157df055e0e65f6abf
Dexter Carpenter <dexter.carpenter03@gmail.com>
2022-04-22 18:36:55 -0700

    Create Joe Script

---
+++ Software/fLO_dAR_Joe/config.h
@@ -0,0 +1,35 @@
+"{\
+	'general':\
+	{\
+		'name':'Device',\
+		'instance':1,\
+		'interval':5000,\
+		'print_verbosity':2\
+	},\
+	'components':[\
+		{\
+			'name':'ADS1115',\
+			'params':'default'\
+		},\
+    {\
+      'name':'MAX31856',\
+      'params':'default'\
+    },\
+		{\
+			'name':'SD',\
+			'params': '[true,1000,10,'data',true]'\
+		},\
+    {\ 
+​      'name':'DS3231',\ 
+​      'params':[11, false, true]\ 
+​    },\
+		{\
+			'name':'Interrupt_Manager',\
+			'params':[0]\
+		},\
+		{\
+			'name':'Sleep_Manager',\
+			'params':[true,false,1]\
+		}\
+	]\
+}"
